3708d187c5f2c165c3fc181ac5616dc845b40841
jennandrsn <jennandrsn@gmail.com>
2023-02-13 18:50:58 -0800

    confused.

--- shiny_script.R
+++ shiny_script.R
@@ -1,43 +1,26 @@
 library(tidyverse)
 library(shiny)
-library(palmerpenguins)
+library(readxl)
+library(here)
+library(lubridate)
 
-# Creating the user interface
+# Read in data?
+coral_data <- read_excel(here("data", "coral_data.xls"))
+
+# Create the user interface:
 ui <- fluidPage(
-  titlePanel("I am adding a title!"), # This is the title!
-  sidebarLayout( # Adding a sidebar & main panel
-    sidebarPanel("put my widgets here",
-                 radioButtons(inputId = "penguin_species", label = "Choose penguin species", choices = c("Adelie","Gentoo","Cool Chinstrap Penguins!" = "Chinstrap"), # This is my first widget for penguins species
-                 ),
-                 selectInput(inputId = "pt_color", label = "Select point color", choices = c("Awesome red!" = "red", "Pretty purple" = "purple", "ORAAANGE" = "orange"))
-    ),
-    mainPanel("put my graph here", # Adding things to the main panel
-              plotOutput(outputId = "penguin_plot")
-    )
+  titlePanel("I am adding a title!"),
+  sidebarLayout(
+    sidebarPanel("put my widgets here"),
+    radioButtons(inputId = "genus", label = "Choose coral species", choices = c("Groovy coral" = "poc","Funky coral" = "acr"))
+    mainPanel("put my graph here")
   )
 )
 
-# Building the server:
-server <- function(input, output) {
 
-  penguin_select <- reactive({
-    penguins %>%
-      filter(species == input$penguin_species)
-  })
+# Create the server function:
+server <- function(input, output) {}
 
-  # Create a reactive plot, which depends on 'species' widget selection:
-
-  output$penguin_plot <- renderPlot({
-
-    ggplot(data = penguin_select(), aes(x = flipper_length_mm, y = body_mass_g)) +
-      geom_point(color = input$pt_color)
-
-  })
-
-}
+# Combine them into an app:
 shinyApp(ui = ui, server = server)
-
-
-
-
 
