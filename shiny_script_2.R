4bc4ab70f3ea86806b213f20d9f08d42be4e9885
jennandrsn <jennandrsn@gmail.com>
2023-02-25 12:33:28 -0800

    nav bar not working, but app is running

--- shiny_script_2.R
+++ shiny_script_2.R
@@ -4,6 +4,8 @@ library(readxl)
 library(here)
 library(lubridate)
 library(shinythemes)
+library(basemaps)
+
 
 
 # Read in data?
@@ -63,8 +65,19 @@ server <- function(input, output) {
   })
 }
 
+ggplot() +
+  geom_sf(data = ca_counties_sf) +
+  geom_sf(data = ca_dgw_sf, aes(color = dgbs)) +
+  scale_color_gradient(low = 'chartreuse', high = 'magenta')
+
+tmap_mode('view')
+tm_shape(ca_dgw_sf) +
+  tm_dots('dgbs', palette = 'Blues')
+
 #Combine above into an app:
 shinyApp(ui = ui, server = server)
+
+
 
 
 #sliderInput(inputId = "perc_bleach",
@@ -73,3 +86,46 @@ shinyApp(ui = ui, server = server)
             #locale = "us", ticks = TRUE, animate = FALSE)
 #output$coral_slider <- renderPrint ({ input$perc_bleach})
 #textOutput('perc_bleach')
+
+
+navbarPage("Map of Moorea",
+           tabPanel("Project Overview",
+                    titlePanel(h2("Map of Moorea!", align = "center")),
+                    titlePanel(h3("Created By: Jenna + Ooooooolivia", align = "center")),
+                    br(),
+                    br(),
+                    br(),
+                    fixedRow(
+                      column(8,
+                             h1("Project Description", align = "center"),
+                             p("Data was collected by the other Olivia something something description.",
+                             ),
+                             br(),
+                             p("More text here about Moorea."),
+                             br(),
+                             p("More text here about corals."),
+                             br(),
+                             p("More text here about how data was collected.",
+                             ),
+                             br(),
+                             p("The purpose of this Shiny App is to.... MAKE CORALS FIGHT!"),
+                             br(),
+                             h2("Data Citation:", align = "center"),
+                             p("1. Cite data"),
+                             br(),
+                             p("2. Cite data"),
+                             br(),
+                             p("3. Cite data"),
+                      ), # end column
+                      column(4,
+                             br(),
+                             br(),
+                             br(),
+                             br(),
+                             img(src = "insertimage.jpg", width = 450),
+                      ), # end column
+                    ), # end fixedrow
+           ), # end tabPanel
+) #end navbar
+
+
