391bc507844d85aa2cebd946ac492079b729759f
osomhegyi <oliviasomhegyu@ucsb.edu>
2023-03-09 16:35:54 -0800

    small fixes from office hours

--- Olivia_Working_Script.R
+++ Olivia_Working_Script.R
@@ -13,7 +13,7 @@ library(ggspatial)
 library(janitor)
 library(ggplot2)
 library(plotly)
-
+library(DT)
 
 
 # Read in data
@@ -60,21 +60,35 @@ ggplot(data = c_sf) +
   geom_sf()
 
 
+###color_list <- list(poc = c('white', '#69b3a2'),
+###                   acr = c('white', 'red'))
+
+
+###add background: bg = 'antiquewhite'
 
 #user interface:
 ui <- navbarPage("Moorea Corals", theme = shinytheme("readable"),
                  tabPanel("Map of Moorea",
-                          titlePanel("Map of Moorea"),
-                          mainPanel(plotlyOutput("map", height=850, width=850))
+                          titlePanel("Moorea, French Polynesia"),
+                          mainPanel(fluidRow(
+                            column(
+                              em("This map respresents 12 sites in the Northshore Lagoon of
+                                      the Moorea Coral Reef Long-term Ecological Research (MCR LTER) site"),
+                              width = 12),
+                            br(),
+                            br(),
+                            plotlyOutput("map", height=650, width=650)
+                          )),
+
 
                  ),
                  tabPanel("Spatial Distribution of Coral Samples",
                           titlePanel("Spatial Distribution of Coral Samples"),
                           # leafletOutput("locations", width = "100%", height = "100%"),
                           sidebarLayout(
-                            sidebarPanel("Selector Variable",
+                            sidebarPanel("Select Variable:",
                                          radioButtons(inputId = "genus_select",
-                                                      label = "Species",
+                                                      label = h3("Species"),
                                                       choices = c("Pocillopora" = "poc","Acropora" = "acr")),
                                          selectInput(inputId = "site_select",
                                                      label = h3("Site Number"),
@@ -85,9 +99,9 @@ ui <- navbarPage("Moorea Corals", theme = shinytheme("readable"),
                                                      choices = unique(coral_grid$plot),
                                                      selected = 1)
                             ),
-                            mainPanel(plotOutput("grid"))
-
-                          )
+                            mainPanel(plotOutput("grid")
+                            ),
+                          ),
                  ),
                  tabPanel("Coral Plot",
                           sidebarLayout(
@@ -107,23 +121,29 @@ ui <- navbarPage("Moorea Corals", theme = shinytheme("readable"),
                                       ##plotOutput(outputId = "coral_plot"),
                                       DT::dataTableOutput(outputId = "coral_table"))
                           )),
-                 tabPanel("Info & Data Sources",
-                          titlePanel("Works Cited"),
-                          fluidRow(column(tags$img(src="poc.jpg",width="300px",height="200px"), width=5),
-                                   br(),
-                                   column(tags$img(src = "acr.jpg",width = "300px",height="200px"), width=5),
-                                   br(),
-                                   column(
-                                     br(),
-                                     br(),
-                                     p("INFO ABOUT CORAL - These corals are reef builders, coral reefs are home to 25% of oceanic life, climate change is affecting this, island of moorea is a long-term coral monitering site, etc.",style="text-align:justify;color:black;background-color:lavender;padding:15px;border-radius:10px"),
-                                     br(),
-                                     br(),
-                                     p("INFO ABOUT DATA - Collected between July 1 2022 and August 26 2022",style="text-align:justify;color:black;background-color:papayawhip;padding:15px;border-radius:10px"),
 
-                                     width=8),
+                 tabPanel("Info & Data Sources",
+                          titlePanel("About"),
+                          fluidRow(
+                            tags$img(src="divers.jpg",width="800px",height="500px"),
+                            width = 12,
+                            align = "left"),
+                          em("Researchers at the Moorea Coral Reef Long-term Ecological Research (LTER) site", align = "center"),
+                          column(
+                            br(),
+                            br(),
+                            p("Coral reefs are among the most diverse and productive ecosystems on Earth.
+                              The Moorea Coral Reef Long-term Ecological Research (MCR LTER) site was established
+                              by the National Science Foundation in 2004 as a model system to better understand
+                              factors that mediate coral community structure and function.This app seeks to visualize
+                              data collected in the Moorea Northshore Lagoon by LTER researcher and PhD student
+                              Olivia Isbell from 7/1/22 - 8/26/22 to help researchers understand if there are any
+                              spatial patterns surveyed or corals Acropora and Pocilopora, either random or clustered."),
+                            width = 12,
+                            align = "left"
                           )
                  ))
+
 
 #Server function:
 server <- function(input, output) {
@@ -136,19 +156,19 @@ server <- function(input, output) {
   output$map <- renderPlotly({
     ggplot(data=fp)+
       geom_sf()+
+      #theme(panel.background = element_rect(fill = 'lightblue', color = 'purple'))+ #this is if we want to change background color
       theme_minimal() +
       coord_sf(xlim=c(-149.70,-149.95),ylim=c(-17.42,-17.62))+
       annotation_scale(
         location = "bl",
         width_hint = 0.2
       ) +
-      geom_sf(data = c_sf, aes(color = site, label=total_corals_found))+
+      geom_sf(data = c_sf, color = "#69b3a2", size = 1.3, aes(labels=total_corals_found))+
       theme(legend.position = "none")+
       coord_sf(xlim=c(-149.70,-149.95),ylim=c(-17.42,-17.62))
   })
 
-  ### Casey's notes from OH for Moorea Map: when can add hover labels (once we update data) and say: color = site, label = genus, poc, acr
-  ### On our hover labels we could show: plot number, number of acr and poc found at each site, northing/easting (if this makes sense?)
+  ### Paste0("Site Number", site, "\n", "Number of Corals:", total_corals_found)
 
 
 
@@ -160,6 +180,18 @@ server <- function(input, output) {
   # addMarkers("long" =~LONGITUDE, "lat" =~LATITUDE) %>%
   # addProviderTiles(providers$Esri.WorldStreetMap)
   # })
+
+
+
+###test colors
+
+##species_color <- reactive({
+##  color_list %>%
+##    pluck(input$genus_select)
+##})
+
+ ###stop test colors
+
 
   # grimes help for tab2
   makefiver <- reactive({
@@ -204,7 +236,8 @@ server <- function(input, output) {
     ggplot(makefiver(), aes(Var1, Var2)) +
       geom_tile(aes(fill = value)) +
       geom_text(aes(label = value)) +
-      scale_fill_gradient(low = "white", high = "red") +
+      #scale_fill_gradientn(colors = species_color())
+      scale_fill_gradient(low = "white", high = "#69b3a2") +
       theme_void()
 
   })
@@ -219,14 +252,15 @@ server <- function(input, output) {
         mean_length = mean(length),
         mean_width = mean(width),
         mean_area = mean(area),
-        mean_perc_dead = mean(perc_dead),
-        mean_perc_bleached = mean(perc_bleach),
+        mean_perc_dead = (mean(perc_dead)/100),
+        mean_perc_bleached = (mean(perc_bleach)/100)
       )
   })
   # tab 3 table
   output$coral_table <- DT::renderDataTable({
-    coral_table() #%>%
-    #formatRound(columns = c(3:9), digits = 2)
+    datatable(coral_table()) %>%
+      formatPercentage(c("mean_perc_dead", "mean_perc_bleached"), 2) %>%
+      formatRound(c(2:4), digits = 2)
   })
 
   ### 3/8 Notes: Need to round to 2 decimals, need to adjust page layout so that "select species" box is smaller.
